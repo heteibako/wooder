a1b4610077697c63310ea6e203126a29a797616b
heteibako <hetei_bako@yahoo.com>
2017-07-04 11:10:15 +0200

    Scroll Reveal
    Scroll reveal added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,48 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+  duration: 2000,
+  origin: "left",
+  easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)'
+}, 100);
 
+sr.reveal('.image1', {
+  duration: 1500,
+  origin: "right",
+  distance: "300px",
+  viewFactor: 1
+}, 100);
 
+sr.reveal('.image2', {
+  duration: 1500,
+  origin: "left",
+  distance: "300px",
+  viewFactor: 1
+}, 100);
+
+sr.reveal('.info_furniture', {
+  duration: 1500,
+  origin: "left",
+  distance: "300px",
+  viewFactor: 1
+}, 100);
+
+sr.reveal('.divider_furniture', {
+  duration: 1400,
+  origin: "left",
+  distance: "300px",
+  viewFactor: 1
+}, 100);
+
+sr.reveal('.info_decor', {
+  duration: 1500,
+  origin: "right",
+  distance: "300px",
+  viewFactor: 1
+}, 100);
+
+sr.reveal('.divider_decor', {
+  duration: 1400,
+  origin: "right",
+  distance: "300px",
+  viewFactor: 1
+}, 100);
